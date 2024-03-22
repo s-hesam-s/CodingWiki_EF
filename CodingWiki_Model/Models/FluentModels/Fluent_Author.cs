a927b0e83585f5377149758f20ba239d37ff6cf4
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-23 00:26:23 +0330

    Section 5 - Add Fluent_Author And Publisher To Db

--- CodingWiki_Model/Models/FluentModels/Fluent_Author.cs
+++ CodingWiki_Model/Models/FluentModels/Fluent_Author.cs
@@ -10,16 +10,11 @@ namespace CodingWiki_Model.Models.FluentModels
 {
     public class Fluent_Author
     {
-        [Key]
         public int Author_Id { get; set; }
-        [Required]
-        [MaxLength(50)]
         public string FirstName { get; set; }
-        [Required]
         public string LastName { get; set; }
         public DateTime BirthDate { get; set; }
         public string Location { get; set; }
-        [NotMapped]
         public string FullName
         {
             get
