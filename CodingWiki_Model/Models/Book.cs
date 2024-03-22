255137ecad20b6cfc61de38483ca5656f7b303b6
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-22 13:52:42 +0330

    Section 2 - Add Migration

--- CodingWiki_Model/Models/Book.cs
+++ CodingWiki_Model/Models/Book.cs
@@ -8,7 +8,8 @@ namespace CodingWiki_Model.Models
 {
     public class Book
     {
-        public int IDBook { get; set; }
+        // [Key]
+        public int BookId { get; set; }
         public string Title { get; set; }
         public string ISBN { get; set; }
         public double Price { get; set; }
