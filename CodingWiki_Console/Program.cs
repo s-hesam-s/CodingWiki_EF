0b83d13393a92075af532176b939fadfbbafbf4a
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-28 20:34:50 +0330

    Section 6 - Get All Books

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -12,3 +12,14 @@ Console.WriteLine("Hello, World!");
 //        context.Database.Migrate(); 
 //    }
 //}
+
+GetAllBooks();
+void GetAllBooks()
+{
+    using var context = new ApplicationDbContext();
+    var books = context.Books.ToList();
+    foreach (var book in books)
+    {
+        Console.WriteLine(book.Title + " - " + book.ISBN);
+    }
+}
