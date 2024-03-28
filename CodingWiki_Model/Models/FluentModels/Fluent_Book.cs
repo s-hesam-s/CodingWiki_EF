64e1eaa4704630716857e9fc435caf5446ba194c
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-28 19:34:04 +0330

    Section 5 - One to Many relation

--- CodingWiki_Model/Models/FluentModels/Fluent_Book.cs
+++ CodingWiki_Model/Models/FluentModels/Fluent_Book.cs
@@ -18,9 +18,8 @@ namespace CodingWiki_Model.Models.FluentModels
 
         public Fluent_BookDetail BookDetail { get; set; }
 
-        //[ForeignKey("Publisher")]
-        //public int Publisher_Id { get; set; }
-        //public Fluent_Publisher Publisher { get; set; }
+        public int Publisher_Id { get; set; }
+        public Fluent_Publisher Publisher { get; set; }
 
         //public List<Fluent_BookAuthorMap> BookAuthorMap{ get; set; }
     }
