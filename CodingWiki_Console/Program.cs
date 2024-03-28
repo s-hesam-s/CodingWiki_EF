4372c3614e320bfffe0a71cd4adf9647fcc21f7d
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-28 20:39:49 +0330

    Section 6 - Creating Book

--- CodingWiki_Console/Program.cs
+++ CodingWiki_Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
 using Microsoft.EntityFrameworkCore;
 
 Console.WriteLine("Hello, World!");
@@ -13,7 +14,9 @@ Console.WriteLine("Hello, World!");
 //    }
 //}
 
+AddBook();
 GetAllBooks();
+
 void GetAllBooks()
 {
     using var context = new ApplicationDbContext();
@@ -22,4 +25,12 @@ void GetAllBooks()
     {
         Console.WriteLine(book.Title + " - " + book.ISBN);
     }
+}
+
+void AddBook()
+{
+    Book book = new() { Title = "New EF Core Book", ISBN = "1231231212", Price = 10.93m, Publisher_Id = 1 };
+    using var context = new ApplicationDbContext();
+    var books = context.Books.Add(book);
+    context.SaveChanges();
 }
