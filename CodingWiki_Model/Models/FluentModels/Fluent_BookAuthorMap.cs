d9aa2f3998adc4990bd2f60437066863ff2f9859
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-28 20:03:43 +0330

    Section 5 - Many to Many Manual Table

--- CodingWiki_Model/Models/FluentModels/Fluent_BookAuthorMap.cs
+++ CodingWiki_Model/Models/FluentModels/Fluent_BookAuthorMap.cs
@@ -8,12 +8,10 @@ namespace CodingWiki_Model.Models.FluentModels
 {
     public class Fluent_BookAuthorMap
     {
-        //[ForeignKey("Book")]
         public int Book_Id { get; set; }
-        //[ForeignKey("Author")]
         public int Author_Id { get; set; }
 
-        //public Fluent_Book Book { get; set; }
-        //public Fluent_Author Author { get; set; }
+        public Fluent_Book Book { get; set; }
+        public Fluent_Author Author { get; set; }
     }
 }
