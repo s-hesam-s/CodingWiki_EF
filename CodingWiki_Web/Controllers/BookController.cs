88a95f7d95e52ca5b555fbab2daf188c9f79384b
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-29 22:03:22 +0330

    Section 8 - List all Books

---
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Book> objList = _db.Books.ToList();
+            return View(objList);
+        }
+    }
+}
