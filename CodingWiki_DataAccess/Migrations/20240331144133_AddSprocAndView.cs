f034e10d128d1c9fcb0990b3b918b4deb9bca42d
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-31 18:26:53 +0330

    Secion 10 - Create Sproc and View

---
+++ CodingWiki_DataAccess/Migrations/20240331144133_AddSprocAndView.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSprocAndView : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"CREATE OR ALTER VIEW dbo.GetMainBookDetails
+                AS
+                SELECT  m.ISBN,m.Title,m.Price FROM dbo.Books m
+            ");
+
+            migrationBuilder.Sql(@"CREATE OR ALTER VIEW dbo.getAllBookDetails
+                AS
+                SELECT * FROM dbo.Books m
+            ");
+
+            migrationBuilder.Sql(@"CREATE PROCEDURE dbo.getBookDetailById
+                    @bookId int
+                AS   
+                    SET NOCOUNT ON;  
+                    SELECT  *  FROM dbo.Books b
+	                WHERE b.BookId=@bookId
+                GO  
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP VIEW dbo.GetMainBookDetails");
+            migrationBuilder.Sql("DROP VIEW dbo.getAllBookDetails");
+            migrationBuilder.Sql("DROP PROCEDURE dbo.getBookDetailById");
+        }
+    }
+}
