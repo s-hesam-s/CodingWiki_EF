a24c2cdd6b09ce601d03e8714f1392c0e866cdb0
s-hesam-s <hesamshoushtarian@gmail.com>
2024-03-31 18:07:04 +0330

    Section 10 - Tracking Vs No Tracking

--- CodingWiki_Web/Program.cs
+++ CodingWiki_Web/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
+          //options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
           options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
